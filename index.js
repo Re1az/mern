303467c9e42009e9ada0066e77b50ef65db60a00
Riwa z <riw4532@gmail.com>
2025-10-28 15:27:07 +0545

    index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+// const personName = 'Remo'
+// const age = 90;
+// const greet = 'hi how are you!'
+
+// console.log(`${personName} ${age} ${greet}`);
+
+// console.log(greet.charAt(1));
+// console.log(greet.includes('h'));
+// console.log(greet.indexOf('h'));
+// console.log(greet.trim().length);
+// console.log(greet.replace('hi' , 'hello'));
+// console.log(greet.replaceAll('hi', 'hello'));
+// console.log(greet.substring(0, 2));
+// const netsalary = 100000;
+// const netexpenses = 50000;
+// if (netsalary > netexpenses) {
+//     console.log{ `you have saved `.$(netsalary - netexpenses) };
+
+//  }
+
+//mutable , imutable
+const per = 'laxman'
+per[0]='k' //imutable
+console.log(per)
+
+const person = ['hari', 'ram', 'shyam']
+person[0]='gita' //mutable
+console.log(person);
+
+
