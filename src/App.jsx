10cce9d83bbdb99d6e97bbff303ea8321b58ae7e
Re1az <riw4532@gmail.com>
2025-12-23 18:57:05 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  const name = "ram";
+  const age = 90;
+
+  return (
+    <div>
+      <h1>Hello World!!!</h1>
+      <p>{`Hello my name is ${name} and I am ${age} years old.`}</p>
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const age = 100;
+
+export const person = {
+  name: "Hari",
+  age: 100,
+};
