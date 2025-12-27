6769117da7fce76e1b649d4d05a354ba75c6f7f5
Re1az <riw4532@gmail.com>
2025-12-27 23:11:16 +0545

    Using components and learning tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,55 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import FelxibleCard from "./components/FelxibleCard";
+import { HiDesktopComputer } from "react-icons/hi";
+import { FaCss3 } from "react-icons/fa";
+import { GoRuby } from "react-icons/go";
+
 export default function App() {
-  const handleEvenOdd = () => {
-    const number = Number(prompt("What is the number?"));
-
-    if (isNaN(number)) {
-      alert("Please enter a valid number");
-      return;
-    }
-
-    if (number % 2 === 0) {
-      alert("The number is even");
-    } else {
-      alert("The number is odd");
-    }
-  };
-
   return (
-    <div className="p-5">
-      <button
-        onClick={handleEvenOdd}
-        className="bg-black text-white rounded-lg p-3 cursor-pointer"
+    <div>
+      <NavBar />
+      <div
+        className="grid grid-cols-1 sm:grid-cols-2 
+                      lg:grid-cols-3 "
       >
-        Click Me
-      </button>
+        <FelxibleCard
+          title={"Responsive"}
+          icon={<HiDesktopComputer size={100} color="#333" />}
+          discription={[
+            "Built-in responsiveness",
+            "Mobile first fluid grid",
+            "Fits any screen sizes",
+            "PC Tablet and Mobile",
+          ]}
+        />
+        <FelxibleCard
+          title={"Standard CSS"}
+          icon={<FaCss3 size={100} color="#333" />}
+          discription={[
+            "Standard CSS only",
+
+            "Easy to learn",
+
+            "No need for jQuery",
+
+            "No JavaScript library",
+          ]}
+        />
+        <FelxibleCard
+          title={"Responsive"}
+          icon={<GoRuby size={100} color="#333" />}
+          discription={[
+            "Paper like design",
+
+            "Bold colors and shadows",
+
+            "Equal across platforms",
+
+            "Equal across devices",
+          ]}
+        />
+      </div>
     </div>
   );
 }
