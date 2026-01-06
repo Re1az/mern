acde14cd517f32efdaecec6e2eb804ea25bd3e66
Re1az <riw4532@gmail.com>
2026-01-06 14:56:44 +0545

    react all push

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,32 @@
-import React from "react";
-import NavBar from "./components/NavBar";
-import FelxibleCard from "./components/FelxibleCard";
-import { HiMiniComputerDesktop } from "react-icons/hi2";
-import { FaCss3 } from "react-icons/fa";
-import { MdOutlineDiamond } from "react-icons/md";
-import ColorClass from "./components/ColorClass";
-import Resize from "./components/Resize";
-import Container from "./components/Container";
-import Card from "./components/Card";
-import Map from "./components/Map";
-import Movie from "./components/Movie";
-
 export default function App() {
   return (
     <div>
-      <NavBar />
-      <div className="grid grid-cols-3 p-10 ">
-        <FelxibleCard
-          title={"Responsive"}
-          icon={
-            <i>
-              <HiMiniComputerDesktop size={150} />
-            </i>
-          }
-          discription={[
-            "Built-in responsiveness",
+      <Child
+        name={"ram"}
+        age={90}
+        address={{
+          city: "bangalore",
+          state: "karnataka",
+        }}
+        jobs={["developer", "tester"]}
+      />
+    </div>
+  );
+}
 
-            "Mobile first fluid grid",
-
-            "Fits any screen sizes",
-
-            "PC Tablet and Mobile",
-          ]}
-        />
-        <FelxibleCard
-          title={"CSS Standard"}
-          icon={
-            <i>
-              <FaCss3 size={150} />
-            </i>
-          }
-          discription={[
-            "Standard CSS only",
-
-            "Easy to learn",
-
-            "No need for jQuery",
-
-            "No JavaScript library",
-          ]}
-        />
-        <FelxibleCard
-          title={"Design"}
-          icon={
-            <i>
-              <MdOutlineDiamond size={150} />
-            </i>
-          }
-          discription={[
-            "Paper like design",
-
-            "Bold colors and shadows",
-
-            "Equal across platforms",
-
-            "Equal across devices",
-          ]}
-        />
-      </div>
-      <ColorClass />
-
-      <Resize />
-      <Container />
-      <Card />
-      <Map />
-      <Movie />
+function Child({ name, age, address, jobs }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{age}</h2>
+      <h3>{address.city}</h3>
+      <h3>{address.state}</h3>
+      <h4>
+        {" "}
+        {jobs.map((item) => {
+          return <li key={item.id}>{item}</li>;
+        })}
+      </h4>
     </div>
   );
 }
